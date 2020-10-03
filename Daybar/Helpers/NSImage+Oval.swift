f22b4d5e8ea7cf6e01f304b15c42c27f1fa6711d
Jay Stakelon <stakes@users.noreply.github.com>
2020-10-03 16:04:58 -0700

    oops update the name also testing a default landing view

--- Daybar/Helpers/NSImage+Oval.swift
+++ Daybar/Helpers/NSImage+Oval.swift
@@ -1,6 +1,6 @@
 //
 //  NSImage+Oval.swift
-//  MacTodayTest
+//  Daybar
 //
 //  Created by Jay Stakelon on 8/22/20.
 //  Copyright © 2020 Jay Stakelon. All rights reserved.
--- Daybar/Views/ContentView.swift
+++ Daybar/Views/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  MacTodayTest
+//  Daybar
 //
 //  Created by Jay Stakelon on 8/16/20.
 //  Copyright © 2020 Jay Stakelon. All rights reserved.
@@ -12,36 +12,47 @@ struct ContentView: View {
     @EnvironmentObject var googleLoader: GoogleLoader
     let eventListViewModel = EventListViewModel()
     var body: some View {
-        Group {
-            VStack(alignment: .leading, spacing: 0) {
-                if (!googleLoader.signedIn) { // not logged in
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        Text("Daybar")
-                            .font(Font.system(size: 15, weight: .medium, design: .default))
-                        Button(action: {
-                            self.googleLoader.signIn()
-                        }) {
-                            Text("Sign in with Google")
-                        }.buttonStyle(signInWithGoogleButtonStyle())
-                        Spacer()
-                    }
-                    Spacer()
-                } else { // logged in
-                    VStack(alignment: .leading, spacing: 0) {
-                        HeaderView(eventListViewModel: eventListViewModel)
-                        EventListView(eventListViewModel: eventListViewModel)
-                    }
-                }
-                Spacer()
-            }
-        }.onAppear {
+        VStack {
+            Spacer()
+            Text("Daybar")
+                .font(Font.system(size: 36, weight: .bold, design: .rounded))
+                .foregroundColor(Color.white)
+            Spacer()
         }
+            .frame(maxWidth: .infinity)
+        .background(LinearGradient(gradient: Gradient(colors: []), startPoint: .top, endPoint: .bottom))
+//        Group {
+//            VStack(alignment: .leading, spacing: 0) {
+//                if (googleLoader.signedIn) { // not logged in
+//                    HStack {
+//                        Spacer()
+//                        HStack {
+//                            Spacer()
+//                            Text("Daybar")
+//                                .font(Font.system(size: 15, weight: .medium, design: .default))
+//                            Button(action: {
+//                                self.googleLoader.signIn()
+//                            }) {
+//                                Text("Sign in with Google")
+//                            }.buttonStyle(signInWithGoogleButtonStyle())
+//                            Spacer()
+//                        }
+//                        Spacer()
+//                    }
+//                    .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .top, endPoint: .bottom))
+//
+//                } else { // logged in
+//                    VStack(alignment: .leading, spacing: 0) {
+//                        HeaderView(eventListViewModel: eventListViewModel)
+//                        EventListView(eventListViewModel: eventListViewModel)
+//                    }
+//                }
+//                Spacer()
+//            }
+//        }.onAppear {
+//        }
     }
 }
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
